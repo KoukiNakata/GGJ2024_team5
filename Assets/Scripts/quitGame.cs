71c3e1b11dbee3a0d0c80cda70e50e623fc3d1d3
KoukiNakata <88617991+KoukiNakata@users.noreply.github.com>
2024-01-28 02:10:59 +0900

    ゲーム終了処理作成

---
+++ Assets/Scripts/quitGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class quitGame : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームを終了する
+    /// </summary>
+    public void quitaGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+}

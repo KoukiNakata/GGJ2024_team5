f52fdc4d065b92b3445b78d76c0462e169ba15b2
TakutoWatanabe <m0118288dc@edu.teu.ac.jp>
2024-01-27 12:17:55 +0900

    サウンド仮実装

---
+++ Assets/Scripts/moveSpeedMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveSpeedMonitor : MonoBehaviour
+{
+    public float monitorSpeed;
+    private Vector3 prevPos;
+    private Vector3 nowPos;
+
+    void Start()
+    {
+        prevPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Approximately(Time.deltaTime, 0))
+            return;
+
+        nowPos = transform.position;
+        var velocity = (nowPos - prevPos) / Time.deltaTime;
+        monitorSpeed = velocity.magnitude;
+
+        prevPos = transform.position;
+
+    }
+}

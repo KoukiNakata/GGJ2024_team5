01f0d95fbfe73cd09341a247c32cbf21da1e8896
TakutoWatanabe <m0118288dc@edu.teu.ac.jp>
2024-01-27 20:10:37 +0900

    BGMと敵のアニメーションの処理を追加

---
+++ Assets/Scripts/EnemyAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    public moveSpeedMonitor MSM;
+    public Animator anim;
+    public bool walk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walk = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MSM.monitorSpeed >= 0.1)
+        {
+            walk = true;
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            walk = false;
+            anim.SetBool("walk", false);
+        }
+            
+    }
+}

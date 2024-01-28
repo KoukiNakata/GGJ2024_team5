189a7e151170aacea31531c3e9d63c846650b4ed
M15ALIENWARE\DnGri <takuan2hks@gmail.com>
2024-01-28 14:25:46 +0900

    ゲームオーバー処理の追加

---
+++ Assets/Scripts/TochGameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TochGameOver : MonoBehaviour
+{
+    [SerializeField] GameObject gameDirectorObj;
+    Gamedirector gameDirector;
+    private string PlayertagName="Player";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameDirector=gameDirectorObj.GetComponent<Gamedirector>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag==PlayertagName)
+        {
+            gameDirector.gameOver();
+        }
+    }
+}

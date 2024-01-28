72895498947369fa71c9a3b27725089f1a53fb07
M15ALIENWARE\DnGri <takuan2hks@gmail.com>
2024-01-28 15:37:13 +0900

    Auto stash before merge of "developBranch" and "origin/addGimmic"

--- Assets/Scripts/UIdirector.cs
+++ Assets/Scripts/UIdirector.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.UI;
+//using UnityEngine.UI;
 using TMPro;
 using UnityEngine.UIElements;
 using Unity.VisualScripting;
